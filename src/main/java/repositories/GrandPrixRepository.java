dc64277893f1ce89869faaf07537dfba86cd4f38
antcarmar2 <antocm5@hotmail.com>
2019-05-15 12:16:57 +0200

    Added the creation of administrators and representatives

--- src/main/java/repositories/GrandPrixRepository.java
+++ src/main/java/repositories/GrandPrixRepository.java
@@ -12,13 +12,13 @@ import domain.GrandPrix;
 @Repository
 public interface GrandPrixRepository extends JpaRepository<GrandPrix, Integer> {
 
-	@Query("select gp from GrandPrix where gp.finalMode='1' and gp.cancelled='0'")
+	@Query("select gp from GrandPrix gp where gp.finalMode='1' and gp.cancelled='0'")
 	Collection<GrandPrix> getPublicGrandPrixes();
 
-	@Query("select gp from GrandPrix where gp.finalMode='1' and gp.cancelled='0' and gp.category.id=?1")
+	@Query("select gp from GrandPrix gp where gp.finalMode='1' and gp.cancelled='0' and gp.category.id=?1")
 	Collection<GrandPrix> grandPrixesByCategory(int id);
 
-	@Query("select gp from GrandPrix where gp.finalMode='1' and gp.cancelled='0' and gp.circuit.id=?1")
+	@Query("select gp from GrandPrix gp where gp.finalMode='1' and gp.cancelled='0' and gp.circuit.id=?1")
 	Collection<GrandPrix> grandPrixesByCircuit(int id);
 
 }
